502b69a2f2cb4693caafb62aaba25722cc8ee415
Thiébaud Modoux <tmodoux@pryv.com>
2018-10-09 16:19:44 +0200

    Improve app style

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <template>
-  <v-app>
-    <v-content>
-      <router-view/>
-    </v-content>
-  </v-app>
+  <div id="app">
+    <head>
+      <link
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"
+        rel="stylesheet">
+    </head>
+    <v-app>
+      <v-content>
+        <router-view/>
+      </v-content>
+    </v-app>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'App',
   data: () => ({
-    title: 'Vuetify.js',
+    title: 'App-web-auth3',
   }),
 };
 </script>
+
+<style>
+    #app {
+        font-family: 'Roboto', 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        padding-left: 20px;
+        padding-right: 20px;
+    }
+</style>
