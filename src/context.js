675978bdbe915eb726b1b42b2fe1d8784a207361
Thiébaud Modoux <tmodoux@pryv.com>
2018-10-26 16:21:17 +0200

    Add default appId for standalone reg+reset, parse pryv domain from hostname

--- src/context.js
+++ src/context.js
@@ -7,17 +7,21 @@ class Context {
   }
 
   static init (params) {
-    const domain = params.domain || 'pryv.me';
+    const domain = domainFromUrl() || params.domain || 'pryv.me';
     const appId = params.requestingAppId || 'app-web-auth';
 
     this.language = params.lang || 'en';
-    this.appId = appId;
+    this.appId = appId || 'pryv-app-web-auth-3';
     this.returnURL = params.returnURL;
     this.oauthState = params.oauthState;
     this.permissions = new Permissions(params.requestedPermissions);
     this.pollKey = params.key;
     this.pryv = new Pryv(domain, appId);
   }
+}
+
+function domainFromUrl () {
+  return location.hostname.split('.').slice(1).join('.');
 }
 
 export default Context;
