5db4c90c5042a75e7f0c31fe81a99daa31f81d74
Ilia Kebets <kebetsi@gmail.com>
2020-04-17 14:44:25 +0200

    add pollUrl to accessState so flow doesn't yell

--- src/components/models/AccessStates.js
+++ src/components/models/AccessStates.js
@@ -22,6 +22,7 @@ export type AccessState = {
   url?: string,
   lang?: string,
   poll?: string,
+  pollUrl?: string, // should be removed from register and use "poll" only
   pollKey?: string, // why is this still here?
   oaccessState?: string,
   expireAfter?: number, // to be added
